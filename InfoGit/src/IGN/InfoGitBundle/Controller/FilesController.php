20a4f35f26932193c261b6e3f6eeea9717e04fe5
Nes280 <Nes280@users.noreply.github.com>
2017-01-15 20:01:53 +0100

    petite amélioration

--- InfoGit/src/IGN/InfoGitBundle/Controller/FilesController.php
+++ InfoGit/src/IGN/InfoGitBundle/Controller/FilesController.php
@@ -13,6 +13,7 @@ class FilesController extends Controller
     private $line;
     private $name;
     private $path;
+    private $compteur = 0;
     //private $list;
 
     public function treeAction()
@@ -39,50 +40,49 @@ class FilesController extends Controller
             }
             $paths[$path] = $type."@".$line;
         }
-        $tree = build_tree($paths);
-        $list = build_list($tree);
+        $tree = $this->build_tree($paths);
+        $list = $this->build_list($tree);
         return $this->render('IGNInfoGitBundle:Files:files.html.twig', array('list' => $list));
     }
 
-}
 
-
-
-function build_tree($path_list) {
-    $path_tree = array();
-    foreach ($path_list as $path => $title) {
-        $list = explode('/', trim($path, '/'));
-        $last_dir = &$path_tree;
-        foreach ($list as $dir) {
-            $last_dir =& $last_dir[$dir];
-        }
-        $last_dir['__title'] = $title;
-    }
-    return $path_tree;
-}
-
-function build_list($tree) {
-    $ul = '';
-    foreach ($tree as $key => $value) {
-        $li = '';
-        if (is_array($value)) {
-            if (array_key_exists('__title', $value)) {
-                $tab = split("@", $value['__title']);
-                if($tab[0] == "Directory")
-                {
-                    $t = rand ( 0 , 100000000000 );
-                    $li .= "<input type=\"checkbox\" id=\"c$t\" /><i class=\"glyphicon glyphicon-folder-close\"></i><i class=\"glyphicon glyphicon-folder-open\"></i><label for=\"c$t\"> $key </label>";
-                }
-                else if ($tab[1] <= 1) {
-                    $li .= "<i class=\"glyphicon glyphicon-file\"></i>$key<p>$tab[1] ligne</p>";
-                }
-                else $li .= "<i class=\"glyphicon glyphicon-file\"></i>$key<p>$tab[1] lignes</p>";
-            } else {
-                $li .= "$key";
+    function build_tree($path_list) {
+        $path_tree = array();
+        foreach ($path_list as $path => $title) {
+            $list = explode('/', trim($path, '/'));
+            $last_dir = &$path_tree;
+            foreach ($list as $dir) {
+                $last_dir =& $last_dir[$dir];
             }
-            $li .= build_list($value);
-            $ul .= strlen($li) ? "<li>$li</li>" : '';
+            $last_dir['__title'] = $title;
         }
+        return $path_tree;
     }
-    return strlen($ul) ? "<ul>$ul</ul>" : '';
+
+    function build_list($tree) {
+        $ul = '';
+        foreach ($tree as $key => $value) {
+            $li = '';
+            if (is_array($value)) {
+                if (array_key_exists('__title', $value)) {
+                    $tab = split("@", $value['__title']);
+                    if($tab[0] == "Directory")
+                    {
+                        $t = $this->compteur;
+                        $li .= "<input type=\"checkbox\" id=\"c$t\" /><i class=\"glyphicon glyphicon-folder-close\"></i><i class=\"glyphicon glyphicon-folder-open\"></i><label for=\"c$t\"> $key </label>";
+                        $this->compteur++;
+                    }
+                    else if ($tab[1] <= 1) {
+                        $li .= "<i class=\"glyphicon glyphicon-file\"></i>$key<p>$tab[1] ligne</p>";
+                    }
+                    else $li .= "<i class=\"glyphicon glyphicon-file\"></i>$key<p>$tab[1] lignes</p>";
+                } else {
+                    $li .= "$key";
+                }
+                $li .= $this->build_list($value);
+                $ul .= strlen($li) ? "<li>$li</li>" : '';
+            }
+        }
+        return strlen($ul) ? "<ul>$ul</ul>" : '';
+    }
 }
